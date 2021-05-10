dac507ccc47751113453a62745c25bbfe56e580c
Rohan Basu <basurohan@gmail.com>
2021-05-09 22:59:32 -0400

    intial commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Rohan Basu on 2021-05-08.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
